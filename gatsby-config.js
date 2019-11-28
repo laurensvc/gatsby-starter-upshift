01c19c956dfac8719952c5dfa4ac2aed1bfbd2d2
Laurensvc <laurens.vancauwelaert@unbrace.be>
2019-11-28 16:27:51 +0100

    added config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,54 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/src/markdown-pages`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-sharp`,
+      options: {
+        useMozJpeg: false,
+        stripMetaData: true,
+        defaultQuality: 82,
+        maxWidth: 1500,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        displayName: true,
+      },
+    },
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./src/assets/images/favicon.jpg",
+
+        // WebApp Manifest Configuration
+        dir: "auto",
+        lang: "nl-BE",
+        background: "rgb(33,37,40)",
+        theme_color: "rgb(33,37,40)",
+        display: "standalone",
+        orientation: "any",
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: true,
+          favicons: true,
+          firefox: true,
+          yandex: true,
+          windows: true,
+        },
+      },
+    },
+  ],
 }
