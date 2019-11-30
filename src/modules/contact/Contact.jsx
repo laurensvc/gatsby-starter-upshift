22b78beac263fcd497bf3623df181faaf8ec035a
Laurensvc <laurens.vancauwelaert@unbrace.be>
2019-11-30 12:47:50 +0100

    :sparkles: contact form

---
+++ src/modules/contact/Contact.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Flex, Box } from 'reflexbox';
+// import { Form } from '../../components/form';
+import styled from '../../config/theme';
+
+const Contact = () => (
+  <ContactPageWrapper>
+    <Box width={[1]} p={3}>
+      <ContactPageTitle>Contact formulier</ContactPageTitle>
+      {/* <Form /> */}
+    </Box>
+  </ContactPageWrapper>
+);
+
+export default Contact;
+
+const ContactPageWrapper = styled(Flex)`
+  margin-top: 50px;
+`;
+
+const ContactPageTitle = styled.h1`
+  padding: 0px 12px;
+  margin: 45px auto 25px;
+  max-width: 650px;
+  text-align: center;
+  font-size: 2rem;
+`;
---
+++ src/pages/contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Contact from '../modules/contact/Contact';
+import GlobalCollection from '../config/GlobalCollection';
+
+const contact = () => (
+  <GlobalCollection>
+    <Contact />
+  </GlobalCollection>
+);
+
+export default contact;
